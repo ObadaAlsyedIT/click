17fa3af86a853db2706a1d56c302541c7724baac
ObadaAlsyedIT <82112879+ObadaAlsyedIT@users.noreply.github.com>
2021-09-23 16:56:39 +0300

    Add files via upload

--- ex3.js
+++ ex3.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded" , () =>{//function = () =>//نفس الشيء
+{//function = () =>//نفس الشيء
     // document.querySelectorAll("button").forEach((button) =>{
     //    button.onclick = () =>{
     //       document.querySelector("h1").style.color = button.dataset.color;//اسم بالتاج نفس
@@ -12,11 +12,12 @@ document.addEventListener("DOMContentLoaded" , () =>{//function = () =>//نفس 
     // document.querySelector("form").onsubmit = function(){
     //    var menu = document.querySelector("#tasks");
     //    menu.innerHTML += "<li>obada</li>";
-    // }
-   
-    document.querySelector("form").onsubmit = function(){
+    // }  
+    
+    
+   /* document.querySelector("form").onsubmit = function(){//on kiy up
        var task = document.querySelector("#task");
-
+      
        
        var li = document.createElement("li");
        
@@ -25,10 +26,35 @@ document.addEventListener("DOMContentLoaded" , () =>{//function = () =>//نفس 
        var list = document.querySelector("#tasks");
        
        list.append(li);
+       document.querySelector("#task").value = "";
 
        return false;
      
+    }*/
+    
+    document.addEventListener("DOMContentLoaded" , function(){
+        var sub = document.querySelector("#submit");
+        sub.disabled = true;
+        sub.style.border = '2px solid red';
+        document.querySelector("#task").style.outline = 'none';
+        document.querySelector("#task").style.marginLeft = '600px';
+
+}
+    )};
+    
+    function myfun(){
+        var x = document.querySelector("#submit");
+        var y =document.querySelector("#task")
+        x.disabled = false;
+        x.style.border = '2px solid green';
+        x.style.borderRadius = '25px';
+        y.style.border = '2px solid green ';
+        y.style.borderRadius = '25px';
+      
+
+
+
+
     }
   
  
-});
